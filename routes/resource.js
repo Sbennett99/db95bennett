5483b25c59655ff04be43c372e349c1e92282397
Seth <54418905+Sbennett99@users.noreply.github.com>
2021-11-10 16:42:44 -0600

    Shot 5

--- routes/resource.js
+++ routes/resource.js
@@ -7,26 +7,25 @@ var api_controller = require('../controllers/api_controller');
 var job_controller = require('../controllers/job_controller'); 
  
 /// API ROUTE /// 
- 
+console.log("I made it")
 // GET resources base. 
-router.get('/resource', api_controller.api); 
- 
+router.get('/', api_controller.api); 
+
 /// Job ROUTES /// 
  
 // POST request for creating a Job.  
-router.post('/resource/jobs', job_controller.job_create_post); 
+router.post('/jobs', job_controller.job_create_post); 
  
 // DELETE request to delete Job. 
-router.delete('/resource/jobs/:id', job_controller.job_delete); 
+router.delete('/jobs/:id', job_controller.job_delete); 
  
 // PUT request to update Job. 
-router.put('/resource/jobs/:id', 
-job_controller.job_update_put); 
+router.put('/jobs/:id', job_controller.job_update_put); 
  
 // GET request for one Job. 
-router.get('/resource/jobs/:id', job_controller.job_detail); 
+router.get('/jobs/:id', job_controller.job_detail); 
  
 // GET request for list of all Job items. 
-router.get('/resource/jobs', job_controller.job_list); 
+router.get('/jobs', job_controller.job_list); 
  
 module.exports = router; 
