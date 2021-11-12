117f73f10eee3f9dd43e516d2ccd2795c9cca814
Seth <54418905+Sbennett99@users.noreply.github.com>
2021-11-12 11:09:50 -0600

    edit readme

--- controllers/job_controller.js
+++ controllers/job_controller.js
@@ -13,9 +13,16 @@ exports.job_list = async function(req, res) {
 };
 
 // for a specific Job. 
-exports.job_detail = function(req, res) { 
-    res.send('NOT IMPLEMENTED: Job detail: ' + req.params.id); 
-}; 
+exports.job_detail = async function(req, res) { 
+    console.log("detail"  + req.params.id) 
+    try { 
+        result = await Job.findById( req.params.id) 
+        res.send(result) 
+    } catch (error) { 
+        res.status(500) 
+        res.send(`{"error": document for id ${req.params.id} not found`); 
+    } 
+}
  
 // Handle Job create on POST. 
 exports.job_create_post = async function(req, res) { 
