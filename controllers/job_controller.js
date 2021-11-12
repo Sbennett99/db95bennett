2e5e18ba8f277f6470c81443f85da7d690408498
Seth <54418905+Sbennett99@users.noreply.github.com>
2021-11-12 11:25:49 -0600

    lab12

--- controllers/job_controller.js
+++ controllers/job_controller.js
@@ -31,7 +31,7 @@ exports.job_create_post = async function(req, res) {
     // We are looking for a body, since POST does not have query parameters. 
     // Even though bodies can be in many different formats, we will be picky 
     // and require that it be a json object 
-    // {"costume_type":"goat", "cost":12, "size":"large"} 
+    // {"job_title":"goat", "pay":12, "isHourly":"large"} 
     document.job_title = req.body.job_title; 
     document.pay = req.body.pay; 
     document.isHourly = req.body.isHourly; 
@@ -53,10 +53,24 @@ exports.job_delete = function(req, res) {
 }; 
  
 // Handle Job update form on PUT. 
-exports.job_update_put = function(req, res) { 
-    res.send('NOT IMPLEMENTED: Job update PUT' + req.params.id); 
+exports.job_update_put =  async function(req, res) { 
+    console.log(`update on id ${req.params.id} with body 
+    ${JSON.stringify(req.body)}`) 
+    try { 
+        let toUpdate = await Job.findById( req.params.id) 
+        // Do updates of properties 
+        if(req.body.job_title)  
+               toUpdate.job_title = req.body.job_title; 
+        if(req.body.pay) toUpdate.pay = req.body.pay; 
+        if(req.body.isHourly) toUpdate.isHourly = req.body.isHourly; 
+        let result = await toUpdate.save(); 
+        console.log("Sucess " + result) 
+        res.send(result) 
+    } catch (err) { 
+        res.status(500) 
+        res.send(`{"error": ${err}: Update for id ${req.params.id} failed`); 
+    } 
 }; 
-
 
 // Handle a show all view 
 exports.job_view_all_Page = async function(req, res) { 
