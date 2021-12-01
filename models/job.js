5d79c857841c5a0e94ea9dd8de795c1b63fdca3b
Seth <54418905+Sbennett99@users.noreply.github.com>
2021-12-01 03:33:13 -0600

    assignment 13

--- models/job.js
+++ models/job.js
@@ -1,9 +1,31 @@
 const mongoose = require("mongoose") 
 const jobSchema = mongoose.Schema({ 
- job_title: String, 
- pay: Number, 
- isHourly : Boolean
+ job_title: {
+    type: String,
+    required: true
+ }, 
+ pay: {
+    type: Number,
+    validate: {
+        validator: function(){
+            if(this.isHourly == true){
+                return this.pay > 10
+            }
+            else{
+                return this.pay > 21000
+            }
+        },
+        message: 'Not above Missouri minimum wage sir'
+    },
+    required: true
+ },
+ isHourly : {
+    type: Boolean,
+    required: true
+ }
+
 }) 
+
  
 module.exports = mongoose.model("Job", 
 jobSchema) 
--- routes/job.js
+++ routes/job.js
@@ -1,6 +1,17 @@
 var express = require('express'); 
 const job_controller = require('../controllers/job_controller'); 
 var router = express.Router(); 
+
+// A little function to check if we have an authorized user and continue on 
+// or 
+// redirect to login. 
+const secured = (req, res, next) => { 
+    if (req.user){ 
+      return next(); 
+    } 
+    req.session.returnTo = req.originalUrl;
+    res.redirect("/login"); 
+  } 
  
 /* GET jobs */ 
 router.get('/', job_controller.job_view_all_Page); 
@@ -11,11 +22,11 @@ router.get('/detail', job_controller.job_view_one_Page);
 
 
 /* GET create job page */ 
-router.get('/create', job_controller.job_create_Page); 
+router.get('/create',secured, job_controller.job_create_Page); 
 
 /* GET create update page */ 
-router.get('/update', job_controller.job_update_Page); 
+router.get('/update',secured, job_controller.job_update_Page); 
 
-router.get('/delete', job_controller.job_delete_Page); 
+router.get('/delete',secured, job_controller.job_delete_Page); 
 
 module.exports = router; 
